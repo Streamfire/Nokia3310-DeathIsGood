bcc5620fb698dfeb1f78666ffa8b59236017b679
Streamfire <Streamfire@users.noreply.github.com>
2020-02-04 23:23:55 +0100

    AUDIO EFFEKTE DRIN

---
+++ internal/Scripts/AudioBus.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var effect:AudioStreamPlayer
+
+
+func _ready() -> void:
+	effect = AudioStreamPlayer.new()
+	effect.bus = "Effects"
+	effect.connect("finished", self, "effect_finished")
+	add_child(effect)
+
+func play_effect(path:String):
+	var stream = load(path)
+	AudioServer.set_bus_mute(1, true)
+	effect.stop()
+	effect.stream = stream
+	effect.play()
+
+func effect_finished() -> void:
+	AudioServer.set_bus_mute(1, false)
